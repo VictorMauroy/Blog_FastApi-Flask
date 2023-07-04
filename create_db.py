731ccbada38d6694cdfa2ff5197fd7e373c43c25
VictorMauroy <mauroy.victor@gmail.com>
2023-07-04 14:23:49 +0200

    feat: :sparkles: :bug: fix circular import and can now create database from models

--- create_db.py
+++ create_db.py
@@ -1,14 +1,10 @@
 from sqlalchemy import create_engine
 
-from Models.user_model import UserBase
-from Models.comment_model import CommentBase
-from Models.post_model import PostBase
+from model import Base
 
 database_url = "sqlite:///./database.db"
 engine = create_engine(database_url)
 
 # Generate database from the tables linked to the "Base".
 # Every entity that inherit Base will be obtained and generated
-UserBase.metadata.create_all(engine)
-PostBase.metadata.create_all(engine)
-CommentBase.metadata.create_all(engine)
+Base.metadata.create_all(bind=engine)
---
+++ model.py
@@ -0,0 +1,51 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, Text
+from typing import List
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "users"
+
+    # Primary key and relations
+    id: Mapped[int] = mapped_column(primary_key=True)
+    comments: Mapped[List["Comment"]] = relationship(back_populates='user')
+    posts:Mapped[List["Post"]] = relationship(back_populates='user')
+    
+    # Properties
+    username: Mapped[str] = mapped_column(nullable=False, unique=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(nullable=False)
+    mdp: Mapped[str] = mapped_column(nullable=False)
+
+
+class Comment(Base):
+    __tablename__ = "comments"
+
+    # Primary key and relations
+    id: Mapped[int] = mapped_column(primary_key=True)
+    # User relation
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    user: Mapped["User"] = relationship(back_populates='comments')
+    # Post relation
+    post_id: Mapped[int] = mapped_column(ForeignKey("posts.id"), nullable=False)
+    post: Mapped["Post"] = relationship(back_populates='posts')
+    
+    # Properties
+    message: Mapped[str] = mapped_column(Text, nullable=False)
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    # Primary key and relations
+    id: Mapped[int] = mapped_column(primary_key=True)
+    # User relation
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    user:Mapped["User"] = relationship(back_populates='posts')
+    # Comment relation
+    comments: Mapped[List["Comment"]] = relationship(back_populates='post')
+
+    # Properties
+    title: Mapped[str] = mapped_column(nullable=False)
+    content: Mapped[str] = mapped_column(nullable=False)
